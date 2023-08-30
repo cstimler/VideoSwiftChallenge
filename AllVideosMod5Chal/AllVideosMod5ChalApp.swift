95e84ca16a554397b6a02fda361cf1f4b0790452
cstimler <charles.ces@gmail.com>
2023-08-30 03:49:23 -0700

    Initial Commit

---
+++ AllVideosMod5Chal/AllVideosMod5ChalApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllVideosMod5ChalApp.swift
+//  AllVideosMod5Chal
+//
+//  Created by October2022 on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct AllVideosMod5ChalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllVideosMod5Chal/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AllVideosMod5Chal
+//
+//  Created by October2022 on 8/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
